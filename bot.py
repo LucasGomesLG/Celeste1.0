74f33b2d0e7fc577274e7686d7254c6dae5d0d81
Lucas-b.l.u.e <114190156+LucasGomesLG@users.noreply.github.com>
2023-06-10 13:27:48 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,27 @@
 import discord
-import os
-
+import random
 class MyClient(discord.Client):
     async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
+         activity = discord.Activity(type=discord.ActivityType.watching, name="Robos")
+         await client.change_presence(status=discord.Status.online, activity=activity)
+         print('Logged on as {0}!'.format(self.user))
 
     async def on_message(self, message):
+        #frases do programa choque de cultura
+        conselhos = ['conselho1','conselho2','conselho3','conselho4,conselho5,]
         print('Message from {0.author}: {0.content}'.format(message))
         if message.content == '/conselho':
-            await message.channel.send(f'{message.author.name}  Não dá pra discutir na internet.{os.linesep}Por mais brilhante que seja sua ideia, a outra pessoa pode rebater com a figurinha do Vampeta pelado{os.linesep}que é um argumento muito mais forte.') 
+            await message.channel.send (f'{message.author.name}, {random.choice(conselhos)}')
+        
+  
     async def on_member_join(self,member):
         guild = member.guild
         if guild.system_channel is not None:
             mensagem = f'{member.mention} Acabou de chegar, seja bem vindo'
             await guild.system_channel.send(mensagem)
 
+
 intents = discord.Intents.default()
 intents.members = True
 
-client = MyClient()
-client.run('meu token')
+client = MyClient('meu token')
